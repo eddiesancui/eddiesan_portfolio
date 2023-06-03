309467c119b910ebda87d3c00f56f976bffd3f55
Eddie San C. Cui <115638558+eddiesancui@users.noreply.github.com>
2023-06-04 00:21:41 +0800

    LInkedin Icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,25 +6,29 @@ const Home = () => {
   return (
     <div 
       name="home" 
-      className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+      className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
 
-      <div className='max-w-screen-lg nx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-          <div>
-            <h2>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+          <div className='flex flex-col justify-center h-full'>
+            <h2 className='text-4xl sm:text-7xl font-bold text-white'>
               I'm a Front-End Developer
             </h2>
-            <p>
+            <p className='text-gray-500 py-4 max-w-md'>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non, distinctio eaque. Iusto, fuga! Porro consequuntur quia molestias rerum cupiditate debitis fugiat voluptatibus sint delectus aspernatur minima nam maxime harum eum voluptatem veritatis quae laborum officiis, eos dolores reiciendis repellat nihil perspiciatis. Vel tenetur earum enim at facere omnis, rem velit.
             </p>
             
             <div>
-              <button>
+              <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                 Portfolio
-                <span>
-                  <MdOutlineKeyboardArrowRight />
+                <span className='group-hover:rotate-90 duration-300'>
+                  <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
                 </span>
               </button>
             </div>
+          </div>
+
+          <div>
+            <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
           </div>
       </div>
     </div>
