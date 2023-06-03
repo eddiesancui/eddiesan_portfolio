24653ddea31b1b0ec99c4f58a8196367739f33e8
Eddie San C. Cui <115638558+eddiesancui@users.noreply.github.com>
2023-06-03 23:09:02 +0800

    Home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
-
-  const {nav, setNav} = useState(false);
+  const [ nav, setNav ] = useState(false);
 
   const links = [
     {
       id: 1,
-      link: 'home'
+      link: "home",
     },
     {
       id: 2,
-      link: 'about'
+      link: "about",
     },
     {
       id: 3,
-      link: 'portfolio'
+      link: "portfolio",
     },
     {
       id: 4,
-      link: 'experience'
+      link: "experience",
     },
     {
       id: 5,
-      link: 'contact'
+      link: "contact",
     },
-  ]
+  ];
+
   return (
     <div className="flex justify-between items-center bg-black w-full h-20 px-4 text-white fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Eddie San</h1>
       </div>
 
-      <ul className='hidden md:flex'>
-        {links.map(({id, link}) => (
-          <li 
-            key={id} 
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
             {link}
@@ -44,13 +44,24 @@ const NavBar = () => {
         ))}
       </ul>
 
-      <div 
-        onClick={() => setNav(!nav)} 
-        className="cursor-pointer pr-4 z-10 text-gray-500">
-          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+              <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
+                {link}
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default NavBar
+export default NavBar;
