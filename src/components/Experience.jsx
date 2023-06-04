f73fb2b4fa9df84406cebd203075e786bd7e88fe
Eddie San C. Cui <115638558+eddiesancui@users.noreply.github.com>
2023-06-04 22:08:04 +0800

    Experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import javascript from '../assets/javascript.png'
+import reactImage from '../assets/react.png'
+import nextjs from '../assets/nextjs.png'
+import graphql from '../assets/graphql.png'
+import github from '../assets/github.png'
+import tailwind from '../assets/tailwind.png'
+
+const Experience = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>These are the technologies I've worked with</p>
+        </div>
+
+        <div>
+          <div>
+            <img src={html} alt="" />
+            <p>HTML</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
+// 1:27
